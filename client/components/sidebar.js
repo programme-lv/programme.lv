288acebd33113495cf452081a176b3eb6af8f952
KrisjanisP <krisjanispetrucena@gmail.com>
2022-10-24 12:54:18 +0300

    mode sidebar into components

---
+++ client/components/sidebar.js
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+    return (
+        <div className="d-flex flex-column align-items-center px-3 pt-2 text-white min-vh-100">
+            <h1>
+                <a href="/" className="d-flex min-vh-100 text-white text-decoration-none">
+                    DEIKSTRA
+                </a>
+            </h1>
+        </div>
+    );
+}
