2961681a73d766b2f5ff59109a71591273b32218
KrisjanisP <krisjanispetrucena@gmail.com>
2022-10-24 14:48:59 +0300

    set default language to cpp

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,16 @@ import Sidebar from '../components/sidebar'
 import CodeMirror from '@uiw/react-codemirror';
 import { sublime } from '@uiw/codemirror-theme-sublime'
 import { javascript } from '@codemirror/lang-javascript'
+import { cpp } from '@codemirror/lang-cpp'
 
+const hello_cpp = `#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  cout<<"Hello, world!"<<endl;
+}
+`
 export default function Home() {
   return (
     <div className="container-fluid">
@@ -13,10 +22,10 @@ export default function Home() {
         <div className="col-auto col-md-9 col-xl-10 px-sm-10 px-0">
           <CodeMirror
             className="h-100"
-            value="console.log('hello world!');"
+            value={hello_cpp}
             theme={sublime}
             height='100%'
-            extensions={[javascript({ jsx: true })]}
+            extensions={[cpp()]}
             onChange={(value, viewUpdate) => {
               console.log('value:', value);
             }}
