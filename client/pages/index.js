91bc6415d819a58de46caca3379634ba9c0e850b
KrisjanisP <krisjanispetrucena@gmail.com>
2022-10-24 12:42:10 +0300

    create a sidebar

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,17 @@
 export default function Home() {
   return (
-    <div>
+    <div class="container-fluid">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+          <div className="d-flex flex-column align-items-center px-3 pt-2 text-white min-vh-100">
+            <h1>
+              <a href="/" className="d-flex min-vh-100 text-white text-decoration-none">
+                DEIKSTRA
+              </a>
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
