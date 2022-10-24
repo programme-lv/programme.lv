088aa3e3b194d03d7c3f0fa3e9563b69ac701b1c
KrisjanisP <krisjanispetrucena@gmail.com>
2022-10-24 13:47:53 +0300

    add codemirror code editor

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,24 @@
 import Sidebar from '../components/sidebar'
+import CodeMirror from '@uiw/react-codemirror';
+import { sublime } from '@uiw/codemirror-theme-sublime'
+import { javascript } from '@codemirror/lang-javascript'
 
 export default function Home() {
   return (
-    <div class="container-fluid">
+    <div className="container-fluid">
       <div className="row flex-nowrap">
         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-          <Sidebar/>
+          <Sidebar />
         </div>
+        <CodeMirror
+          value="console.log('hello world!');"
+          theme={sublime}
+          height="100%"
+          extensions={[javascript({ jsx: true })]}
+          onChange={(value, viewUpdate) => {
+            console.log('value:', value);
+          }}
+        />
       </div>
     </div>
   )
