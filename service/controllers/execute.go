dec49032301a3ad6f5302e38cc3853bc67365c5d
KrisjanisP <krisjanispetrucena@gmail.com>
2022-10-30 13:46:12 +0200

    receive execution

--- service/controllers/execute.go
+++ service/controllers/execute.go
@@ -1,9 +1,27 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Execution struct {
+	LangId  string
+	SrcCode string
+	StdIn   string
+}
 
 func EnqueueExecution(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	// TODO read https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
+	var e Execution
+	err := json.NewDecoder(r.Body).Decode(&e)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Execution: %+v", e)
 }
 
 func ListExecutions(w http.ResponseWriter, r *http.Request) {
