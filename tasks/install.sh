e98374233dda20e265b9076dfe69d0264cfe69c2
KrisjanisP <krisjanispetrucena@gmail.com>
2022-11-09 18:16:05 +0200

    polish istall script

--- tasks/install.sh
+++ tasks/install.sh
@@ -12,10 +12,15 @@ fi
 rm -r /srv/deikstra/tasks
 mkdir -p /srv/deikstra/tasks
 
+tasks_dir=/srv/deikstra/tasks
+
 for task in ./*; do
     if [ -d "$task" ]; then
-        (cd $task;./compile.sh)
-        cp -r "$task"/result /srv/deikstra/tasks/$task
-        rm -r $task/result
+        echo "copying task \"$(basename -- $task)\" into $tasks_dir"
+        if [ ! -d "$task/result" ]; then
+            (cd $task;./compile.sh)
+        fi
+        cp -r "$task"/result $tasks_dir/$task
+        # rm -r $task/result
     fi
 done
