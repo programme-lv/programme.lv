b482f0cea637b3a99a7992b5bbab5b797354dc7e
KrisjanisP <krisjanispetrucena@gmail.com>
2022-11-09 19:40:55 +0200

    copy only updated tasks

--- tasks/install.sh
+++ tasks/install.sh
@@ -9,18 +9,27 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
-rm -r /srv/deikstra/tasks
 mkdir -p /srv/deikstra/tasks
 
 tasks_dir=/srv/deikstra/tasks
 
 for task in ./*; do
     if [ -d "$task" ]; then
+        
+        task_name=$(basename -- $task)
+        dest=$tasks_dir/$task_name
+
+        if [ -d "$dest" ] && [ -d "$task/result" ] && [ ! "$task/result" -nt "$dest" ]
+        then
+            echo "task \"$(basename -- $task)\" is up to date"
+            continue
+        fi
+        
         echo "copying task \"$(basename -- $task)\" into $tasks_dir"
         if [ ! -d "$task/result" ]; then
             (cd $task;./compile.sh)
         fi
-        cp -r "$task"/result $tasks_dir/$task
+        cp -r $task/result $dest
         # rm -r $task/result
     fi
 done
