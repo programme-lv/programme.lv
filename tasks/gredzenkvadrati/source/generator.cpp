a7ad6c8204f27f873306777a63938e64c3576cc1
KrisjanisP <krisjanispetrucena@gmail.com>
2022-11-16 11:58:47 +0200

    update generator

--- tasks/gredzenkvadrati/source/generator.cpp
+++ tasks/gredzenkvadrati/source/generator.cpp
@@ -7,11 +7,15 @@ void write_test(int test) {
     startTest(test);
     ll a, b;
     if(test<=10) { // subtask 1
-        a = rnd.next((ll)1, (ll)1e3-1);
+        a = rnd.next((ll)1, (ll)1e2-1);
+        b = rnd.next((ll)a+1, (ll)1e2);
+    }
+    else if(test<=15) { // subtask 2
+        a = rnd.next((ll)1e2, (ll)1e3-1);
         b = rnd.next((ll)a+1, (ll)1e3);
     }
-    else if(test<=20) { // subtask 2
-        a = rnd.next((ll)1, (ll)1e6-1);
+    else if(test<=20) { // subtask 3
+        a = rnd.next((ll)1e4, (ll)1e6-1);
         b = rnd.next((ll)a+1, (ll)1e6);
     }
     println(a,b);
