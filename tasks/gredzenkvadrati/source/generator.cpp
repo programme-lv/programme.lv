33dc49c96bd601750ebed9937b2401ea166a2237
KrisjanisP <krisjanispetrucena@gmail.com>
2022-11-16 11:41:04 +0200

    add more tests

--- tasks/gredzenkvadrati/source/generator.cpp
+++ tasks/gredzenkvadrati/source/generator.cpp
@@ -10,7 +10,7 @@ void write_test(int test) {
         a = rnd.next((ll)1, (ll)1e3-1);
         b = rnd.next((ll)a+1, (ll)1e3);
     }
-    else if(test<=15) { // subtask 2
+    else if(test<=20) { // subtask 2
         a = rnd.next((ll)1, (ll)1e6-1);
         b = rnd.next((ll)a+1, (ll)1e6);
     }
@@ -20,6 +20,6 @@ void write_test(int test) {
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
 
-    for(int i=1;i<=15;i++)
+    for(int i=1;i<=20;i++)
         write_test(i);
 }
