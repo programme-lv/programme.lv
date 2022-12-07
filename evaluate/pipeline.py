e9aee87a0662042c22c756d6ecf9392e7f72be6a
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-07 17:11:04 +0200

    remove folder

--- evaluate/pipeline.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-
-user = await compile(submission)
-for test in tests:
-    with test:
-        output = File(role="answer")
-        user(stdin=test.input, stdout=output)
-        checker(test.input, output, test.answer)
