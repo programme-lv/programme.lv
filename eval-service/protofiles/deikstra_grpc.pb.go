9fe72227d6e6ece0a2165850ff919ca23a5bc48b
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-17 21:34:18 +0200

    update protobuf file

--- eval-service/protofiles/deikstra_grpc.pb.go
+++ eval-service/protofiles/deikstra_grpc.pb.go
@@ -23,6 +23,8 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type SchedulerClient interface {
 	GetJobs(ctx context.Context, in *RegisterWorker, opts ...grpc.CallOption) (Scheduler_GetJobsClient, error)
+	// rpc ReportWorkerStatus(stream ReportWorkerStatusRequest) returns (google.protobuf.Empty) {}
+	ReportJobStatus(ctx context.Context, opts ...grpc.CallOption) (Scheduler_ReportJobStatusClient, error)
 }
 
 type schedulerClient struct {
@@ -65,11 +67,47 @@ func (x *schedulerGetJobsClient) Recv() (*Job, error) {
 	return m, nil
 }
 
+func (c *schedulerClient) ReportJobStatus(ctx context.Context, opts ...grpc.CallOption) (Scheduler_ReportJobStatusClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Scheduler_ServiceDesc.Streams[1], "/protofiles.Scheduler/ReportJobStatus", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &schedulerReportJobStatusClient{stream}
+	return x, nil
+}
+
+type Scheduler_ReportJobStatusClient interface {
+	Send(*JobStatusUpdate) error
+	CloseAndRecv() (*JobAction, error)
+	grpc.ClientStream
+}
+
+type schedulerReportJobStatusClient struct {
+	grpc.ClientStream
+}
+
+func (x *schedulerReportJobStatusClient) Send(m *JobStatusUpdate) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *schedulerReportJobStatusClient) CloseAndRecv() (*JobAction, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(JobAction)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // SchedulerServer is the server API for Scheduler service.
 // All implementations must embed UnimplementedSchedulerServer
 // for forward compatibility
 type SchedulerServer interface {
 	GetJobs(*RegisterWorker, Scheduler_GetJobsServer) error
+	// rpc ReportWorkerStatus(stream ReportWorkerStatusRequest) returns (google.protobuf.Empty) {}
+	ReportJobStatus(Scheduler_ReportJobStatusServer) error
 	mustEmbedUnimplementedSchedulerServer()
 }
 
@@ -79,6 +117,9 @@ type UnimplementedSchedulerServer struct {
 
 func (UnimplementedSchedulerServer) GetJobs(*RegisterWorker, Scheduler_GetJobsServer) error {
 	return status.Errorf(codes.Unimplemented, "method GetJobs not implemented")
+}
+func (UnimplementedSchedulerServer) ReportJobStatus(Scheduler_ReportJobStatusServer) error {
+	return status.Errorf(codes.Unimplemented, "method ReportJobStatus not implemented")
 }
 func (UnimplementedSchedulerServer) mustEmbedUnimplementedSchedulerServer() {}
 
@@ -114,6 +155,32 @@ func (x *schedulerGetJobsServer) Send(m *Job) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _Scheduler_ReportJobStatus_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(SchedulerServer).ReportJobStatus(&schedulerReportJobStatusServer{stream})
+}
+
+type Scheduler_ReportJobStatusServer interface {
+	SendAndClose(*JobAction) error
+	Recv() (*JobStatusUpdate, error)
+	grpc.ServerStream
+}
+
+type schedulerReportJobStatusServer struct {
+	grpc.ServerStream
+}
+
+func (x *schedulerReportJobStatusServer) SendAndClose(m *JobAction) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *schedulerReportJobStatusServer) Recv() (*JobStatusUpdate, error) {
+	m := new(JobStatusUpdate)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // Scheduler_ServiceDesc is the grpc.ServiceDesc for Scheduler service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -126,6 +193,11 @@ var Scheduler_ServiceDesc = grpc.ServiceDesc{
 			StreamName:    "GetJobs",
 			Handler:       _Scheduler_GetJobs_Handler,
 			ServerStreams: true,
+		},
+		{
+			StreamName:    "ReportJobStatus",
+			Handler:       _Scheduler_ReportJobStatus_Handler,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "deikstra.proto",
