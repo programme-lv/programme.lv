b8f72561617410b107bed7c91b4b5cf9aa6c6bf8
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-24 17:55:31 +0200

    navigation bar

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -3,7 +3,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import Head from "next/head";
 
+import {useEffect} from 'react'
+
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+      import("bootstrap/dist/js/bootstrap");
+  },[])
   return (
     <>
       <Head>
--- website/pages/index.js
+++ website/pages/index.js
@@ -1,62 +1,54 @@
-import Sidebar from '../components/sidebar'
-import CodeMirror from '@uiw/react-codemirror';
-import { sublime } from '@uiw/codemirror-theme-sublime'
-import { cpp } from '@codemirror/lang-cpp'
-
-const hello_cpp = `#include <iostream>
-
-using namespace std;
-
-int main() {
-  cout<<"Hello, world!"<<endl;
-}
-`
-
-var written_code = hello_cpp;
-
-function execute_code() {
-  const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ code: written_code })
-  };
-  fetch('http://localhost:8080/execute/enqueue', requestOptions)
-      .then(response => response.json())
-}
-
 export default function Home() {
-  return (
-    <div className="d-flex">
-        <div className="bg-dark">
-          <Sidebar/>
+    return (
+        <div>
+            <nav className="navbar bg-white">
+                <div className="navbar-expand-md container">
+                    <a className="navbar-brand" href="#">programme.lv</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbar-offcanvas">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbar-main">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">uzdevumi</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">rezultāti</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">iesūtījumi</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">sacensības</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">redaktors</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="offcanvas offcanvas-end" id="navbar-offcanvas">
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">uzdevumi</a>
+                            </li>
+                            <li className="nav-item mt-2">
+                                <a className="nav-link" href="#">rezultāti</a>
+                            </li>
+                            <li className="nav-item mt-2">
+                                <a className="nav-link" href="#">iesūtījumi</a>
+                            </li>
+                            <li className="nav-item mt-2">
+                                <a className="nav-link" href="#">sacensības</a>
+                            </li>
+                            <li className="nav-item mt-2">
+                                <a className="nav-link" href="#">redaktors</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
-        <div className="flex-grow-1">
-          <div className="h-75">
-            <CodeMirror
-              className="h-100"
-              value={hello_cpp}
-              theme={sublime}
-              height='100%'
-              extensions={[cpp()]}
-              onChange={(value) => {
-                written_code = value;
-              }}
-            />
-          </div>
-          <div className="h-25 bg-dark p-2 pb-3 d-flex">
-            <div className='col-4 px-3 d-flex flex-column'>
-              <label className="form-label text-white">Standard Input ( stdin )</label>
-              <textarea className="form-control flex-grow-1" spellCheck="false"></textarea>
-            </div>
-            <div className="col-4 px-3 d-flex flex-column">
-              <label className="form-label text-white">Expected Output ( stdout )</label>
-              <textarea className="form-control flex-grow-1" spellCheck="false"></textarea>
-            </div>
-            <div className="col-4 px-3">
-              <button onClick={execute_code} className="col btn btn-success" type="button">Run</button>
-            </div>
-          </div>
-        </div>
-    </div>
-  )
+    )
 }
