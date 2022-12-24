e276be076b5b6c6efb5be9a00836f2e298ce13fc
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-24 18:14:53 +0200

    remove margin

--- website/pages/index.js
+++ website/pages/index.js
@@ -49,9 +49,9 @@ export default function Home() {
                     </div>
                 </div>
             </nav>
-            <main style={{ backgroundImage: "url(space1.jpg)", height: "100vh", backgroundSize: "contain"}}>
-                <div className="container text-white py-5 mt-4">
-                <h2 className="my-5">jauns nostūris informātikas un matemātikas cienītājiem</h2>
+            <main style={{ backgroundImage: "url(space1.jpg)", height: "100vh", backgroundSize: "contain" }}>
+                <div className="container text-white py-5">
+                    <h2 className="my-5">jauns nostūris informātikas un matemātikas cienītājiem</h2>
                 </div>
             </main>
             <footer>
