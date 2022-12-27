cab7c738c7390ae1ddda3d3d99db6eba9c97d1fe
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-28 01:02:06 +0200

    delete task modal

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,23 +1,23 @@
-
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import Head from "next/head";
 
 import {useEffect} from 'react'
 
-function MyApp({ Component, pageProps }) {
-  useEffect(()=>{
-      import("bootstrap/dist/js/bootstrap");
-  },[])
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>programme.lv</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+function MyApp({Component, pageProps}) {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap");
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <title>programme.lv</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp
--- website/pages/admin.js
+++ website/pages/admin.js
@@ -43,7 +43,7 @@ async function deleteTask(task_code) {
         body: JSON.stringify({task_code: task_code}),
     };
 
-    const response = await fetch("http://localhost:8080/tasks/delete/"+task_code, fetchOptions);
+    const response = await fetch("http://localhost:8080/tasks/delete/" + task_code, fetchOptions);
 
     if (!response.ok) {
         const errorMessage = await response.text();
@@ -70,17 +70,26 @@ export default function Admin(props) {
         await createTask(e);
         await refreshTable();
     }
-    let deleteTaskAndRefreshTabble = async (task_code) => {
+    let deleteTaskAndRefreshTable = async (task_code) => {
         await deleteTask(task_code);
         await refreshTable();
     }
+    let displayTaskDeleteModal = async (task_name, task_code) => {
+        document.getElementById("delete-task-modal-header").innerHTML=task_name
+        document.getElementById("delete-task-modal-confirm").onclick= async ()=> {
+            await deleteTaskAndRefreshTable(task_code);
+            document.getElementById("delete-task-modal-close").click()
+        }
+        document.getElementById("delete-task-modal-show").click()
 
+    }
     tasks.forEach((task) => {
         admin_table_entries.push(
             <tr key={task["task_code"]}>
                 <th scope="row"><Link href={"/tasks/" + task["task_code"]}><a
                     className="nav-link">{task["task_code"]}</a></Link></th>
-                <td><Link href={"/tasks/" + task["task_code"]}><a className="nav-link">{task["task_name"]}</a></Link>
+                <td><Link href={"/tasks/" + task["task_code"]}><a
+                    className="nav-link">{task["task_name"]}</a></Link>
                 </td>
                 <td><span className="badge bg-primary">ProblemCon++</span></td>
                 <td><span className="badge bg-danger">6.9</span></td>
@@ -88,7 +97,9 @@ export default function Admin(props) {
                 <td>13</td>
                 <td>
                     <button type="button" className="btn btn-sm btn-primary me-1">Rediģēt</button>
-                    <button type="button" className="btn btn-sm btn-danger ms-1" onClick={()=>deleteTaskAndRefreshTabble(task["task_code"])}>Izdzēst</button>
+                    <button type="button" className="btn btn-sm btn-danger ms-1"
+                            onClick={() => displayTaskDeleteModal(task["task_name"], task["task_code"])}>Izdzēst
+                    </button>
                 </td>
             </tr>
         )
@@ -102,10 +113,12 @@ export default function Admin(props) {
                 <form action="http://localhost:8080/tasks/create" onSubmit={createTaskAndRefreshTable}>
                     <div className="row">
                         <div className="mb-3 col">
-                            <input type="text" className="form-control" id="task-code" name="task_code" placeholder={"kods"}/>
+                            <input type="text" className="form-control" id="task-code" name="task_code"
+                                   placeholder={"kods"}/>
                         </div>
                         <div className="mb-3 col">
-                            <input type="text" className="form-control" id="task-name" name="task_name" placeholder={"nosaukums"}/>
+                            <input type="text" className="form-control" id="task-name" name="task_name"
+                                   placeholder={"nosaukums"}/>
                         </div>
                         <div className={"col"}>
                             <button type="submit" className="btn btn-success">pievienot uzdevumu</button>
@@ -129,6 +142,28 @@ export default function Admin(props) {
                     </tbody>
                 </table>
             </main>
+            <button type="button" className="btn btn-primary d-none" id="delete-task-modal-show" data-bs-toggle="modal"
+                    data-bs-target="#delete-task-modal">
+            </button>
+            <div className="modal fade" id="delete-task-modal" tabIndex="-1">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="delete-task-modal-header"></h5>
+                            <button id="delete-task-modal-close" type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+                        <div className="modal-body">
+                            Vai esat pārliecināti, ka vēlaties dzēst šo uzdevumu?
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">aizvērt
+                            </button>
+                            <button type="button" className="btn btn-danger" id="delete-task-modal-confirm">dzēst uzdevumu</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
