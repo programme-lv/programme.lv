fb67c7d662960c4168de506aa3eed16c97f5b7da
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-15 20:29:00 +0200

    display pdf file

--- website/pages/tasks/[id].js
+++ website/pages/tasks/[id].js
@@ -6,14 +6,18 @@ export default function Task(props) {
     const router = useRouter()
     const { id } = router.query
 
+    console.log(props.task["pdf_statements"][0].filename)
+
+    let pdfURL = props.apiURL+"/tasks/statement/" + props.task["code"]+"/"+props.task["pdf_statements"][0].filename
 
     return (
-        <div>
+        <>
             <NavBar active_page={"tasks"} />
-            <main className="container">
+            <main className="container" style={{height: "80vh"}}>
                 <h1 className="my-4 text-center">{id}</h1>
+                <embed src={pdfURL} type="application/pdf" width="100%" height="100%"/>
             </main>
-        </div>
+        </>
     )
 }
 
@@ -21,7 +25,8 @@ export async function getServerSideProps(context) {
     try {
         const reqRes = await fetch(`${process.env.API_URL}/tasks/view/${context.params.id}`)
         const task = await reqRes.json()
-        return { props: { task } }
+        return { props: { task: task,
+                apiURL: process.env.API_URL} }
     } catch (err) {
         console.log(err)
         return { props: { error: "failed to fetch task info from the API :(" } }
