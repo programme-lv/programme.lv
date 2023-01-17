e11453d04132fb7c86e36321d98ebeec22f8c7b6
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-17 11:08:00 +0200

    format [id].js

--- website/pages/tasks/[id].js
+++ website/pages/tasks/[id].js
@@ -3,11 +3,11 @@ import TagList from "../../components/taglist";
 
 
 export default function Task(props) {
-    let pdfURL = props.apiURL+"/tasks/statement/" + props.task["code"]+"/"+props.task["pdf_statements"][0].filename
+    let pdfURL = props.apiURL + "/tasks/statement/" + props.task["code"] + "/" + props.task["pdf_statements"][0].filename
 
     return (
         <>
-            <NavBar active_page={"tasks"} />
+            <NavBar active_page={"tasks"}/>
             <main className="container">
                 <div className={"row my-5"}>
                     <div className="col-9">asdf</div>
@@ -79,10 +79,14 @@ export async function getServerSideProps(context) {
     try {
         const reqRes = await fetch(`${process.env.API_URL}/tasks/view/${context.params.id}`)
         const task = await reqRes.json()
-        return { props: { task: task,
-                apiURL: process.env.API_URL} }
+        return {
+            props: {
+                task: task,
+                apiURL: process.env.API_URL
+            }
+        }
     } catch (err) {
         console.log(err)
-        return { props: { error: "failed to fetch task info from the API :(" } }
+        return {props: {error: "failed to fetch task info from the API :("}}
     }
 }
