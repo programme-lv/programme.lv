9e5af3e3f2bb38822a76791bfaa58876b7401c8a
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-20 21:36:46 +0200

    remove console logs from format datetime

--- website/scripts/format_datetime.js
+++ website/scripts/format_datetime.js
@@ -1,7 +1,5 @@
 export function formatDateTime(dateTime) {
-    console.log("dateTime: ", dateTime);
     let d = new Date(dateTime);
-    console.log(d);
     let date = d.toISOString().split("T")[0];
     date = date.replace("-", ".");
     date = date.replace("-", ".");
