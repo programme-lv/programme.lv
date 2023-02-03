a4d17365c19afa0d8baff451b954f2a097267891
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-03 14:35:43 +0200

    convert to pointers

--- backend/models/task.go
+++ backend/models/task.go
@@ -7,21 +7,21 @@ type Task struct {
 	CreatedAt time.Time `json:"created_time"`
 	UpdatedAt time.Time `json:"updated_time"`
 
-	Name   string    `json:"name" gorm:"not null;unique"`
-	Author string    `json:"author"`
-	Tags   []TaskTag `json:"tags" gorm:"many2many:tasks_tags;"`
+	Name   string     `json:"name" gorm:"not null;unique"`
+	Author string     `json:"author"`
+	Tags   []*TaskTag `json:"tags" gorm:"many2many:tasks_tags;"`
 
-	Type     string   `json:"type" gorm:"not null"`
-	TaskType TaskType `gorm:"foreignKey:Type"`
+	Type     string    `json:"type" gorm:"not null"`
+	TaskType *TaskType `gorm:"foreignKey:Type"`
 
 	TimeLim uint32 `json:"time_lim" toml:"time_lim"`
 	MemLim  uint32 `json:"mem_lim" toml:"mem_lim"`
 
-	Tests    []TaskTest    `json:"tests"`
-	Subtasks []TaskSubtask `json:"subtasks"`
+	Tests    []*TaskTest    `json:"tests"`
+	Subtasks []*TaskSubtask `json:"subtasks"`
 
-	MDStatements  []MarkdownStatement `json:"md_statements"`
-	PDFStatements []PDFStatement      `json:"pdf_statements"`
+	MDStatements  []*MarkdownStatement `json:"md_statements"`
+	PDFStatements []*PDFStatement      `json:"pdf_statements"`
 }
 
 type TaskType struct {
@@ -37,12 +37,12 @@ type TaskTest struct {
 	Name string `json:"name" gorm:"not null"`
 
 	TaskID string `json:"task_id" gorm:"not null"`
-	Task   Task   `json:"task" gorm:"foreignKey:TaskID"`
+	Task   *Task  `json:"task" gorm:"foreignKey:TaskID"`
 
 	Input  string `json:"input"`
 	Answer string `json:"answer"`
 
-	Subtasks []TaskSubtask `json:"subtasks" gorm:"many2many:subtasks_tests"`
+	Subtasks []*TaskSubtask `json:"subtasks" gorm:"many2many:subtasks_tests"`
 }
 
 type TaskSubtask struct {
@@ -54,11 +54,11 @@ type TaskSubtask struct {
 	Pattern string `json:"pattern" gorm:"not null"`
 
 	TaskID string `json:"task_id" gorm:"not null"`
-	Task   Task   `json:"task" gorm:"foreignKey:TaskID"`
+	Task   *Task  `json:"task" gorm:"foreignKey:TaskID"`
 
 	Score int `json:"score" go:"not null"`
 
-	Tests []TaskTest `json:"tests"  gorm:"many2many:subtasks_tests"`
+	Tests []*TaskTest `json:"tests"  gorm:"many2many:subtasks_tests"`
 }
 
 type TaskTag struct {
@@ -69,5 +69,5 @@ type TaskTag struct {
 	Name  string `json:"name" gorm:"unique;not null"`
 	Color string `json:"color"`
 
-	Tasks []Task `json:"tasks" gorm:"many2many:tasks_tags"`
+	Tasks []*Task `json:"tasks" gorm:"many2many:tasks_tags"`
 }
