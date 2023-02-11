17616a6bf19560fb1f9a289aa69580bf0e58484b
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-12 00:07:40 +0200

    larger headers for statement

--- website/components/MDStatement.js
+++ website/components/MDStatement.js
@@ -19,30 +19,30 @@ export default function MDStatement({mdStatement}) {
     }, []);
     return (<>
         <section className="my-4">
-            <h5 className={"my-3"}>formulējums</h5>
+            <h4 className={"my-3"}>formulējums</h4>
             <div dangerouslySetInnerHTML={{__html: description}}></div>
         </section>
         <section className="my-4">
-            <h5>ievaddati</h5>
+            <h4 className={"my-3"}>ievaddati</h4>
             <div dangerouslySetInnerHTML={{__html: input}}></div>
         </section>
         <section className="my-4">
-            <h5>izvaddati</h5>
+            <h4 className={"my-3"}>izvaddati</h4>
             <div dangerouslySetInnerHTML={{__html: output}}></div>
         </section>
         <section className="my-4">
-            <h5>piemēri</h5>
-            <div className={"row"}>
+            <h4 className={""}>piemēri</h4>
+            <div className={"d-flex"}>
                 {examples.map((example, index) =>
                     <ExampleTable key={index} input={example["input"]} output={example["output"]}/>)}
             </div>
         </section>
         {scoring && <section className="my-4 md-statement-scoring">
-            <h5>vērtēšana</h5>
+            <h4 className={"my-3"}>vērtēšana</h4>
             <div dangerouslySetInnerHTML={{__html: scoring}}></div>
         </section>}
         {notes && <section className="my-4">
-            <h5>piezīmes</h5>
+            <h4 className={"my-3"}>piezīmes</h4>
             <div dangerouslySetInnerHTML={{__html: notes}}></div>
         </section>}
     </>)
