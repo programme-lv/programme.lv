616123fa0c48166f8ae03f2950281b13c5292135
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-19 22:06:59 +0200

    remove created at from languages

--- backend/models/language.go
+++ backend/models/language.go
@@ -5,7 +5,6 @@ import "time"
 // Language is managed by GORM
 type Language struct {
 	ID        string    `json:"lang_id" gorm:"primaryKey"`
-	CreatedAt time.Time `json:"created_time"`
 	UpdatedAt time.Time `json:"updated_time"`
 
 	Name       string `json:"name" gorm:"unique;not null"`
