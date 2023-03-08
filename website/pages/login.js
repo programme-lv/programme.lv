f5672d85b93c366f31164c4998e4213b8a13d676
KrisjanisP <krisjanispetrucena@gmail.com>
2023-03-08 23:33:00 +0200

    login page

--- website/pages/login.js
+++ website/pages/login.js
@@ -1,6 +1,8 @@
 import {useState} from 'react';
 import {Envelope, Lock} from 'react-bootstrap-icons';
 import Navbar from "../components/Navbar";
+import Image from 'next/image'
+import Fractal_canopy from '../public/fractal-canopy.png'
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
@@ -14,39 +16,48 @@ const LoginForm = () => {
     return (<>
         <Navbar/>
         <div className="container">
-            <div className="col-5 m-auto border p-4 mt-5 bg-white">
-                <h4 className="text-center mb-3">Pieslēdzies savam kontam</h4>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Epasta adrese</label>
-                        <div className="input-group">
-                            <input type="email" className="form-control" id="email" value={email}
-                                   onChange={(e) => setEmail(e.target.value)} required/>
-                            <span className="input-group-text bg-white"><Envelope/></span>
+            <div className={"d-flex justify-content-around mt-5"}>
+                <div className="col-5 border p-4">
+                    <h4 className="text-center mb-3">Pieslēdzies savam kontam</h4>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Epasta adrese</label>
+                            <div className="input-group">
+                                <input type="email" className="form-control" id="email" value={email}
+                                       onChange={(e) => setEmail(e.target.value)} required/>
+                                <span className="input-group-text bg-white"><Envelope/></span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Parole</label>
-                        <div className="input-group">
-                            <input type="password" className="form-control" id="password" value={password}
-                                   onChange={(e) => setPassword(e.target.value)} required/>
-                            <span className="input-group-text bg-white"><Lock/></span>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Parole</label>
+                            <div className="input-group">
+                                <input type="password" className="form-control" id="password" value={password}
+                                       onChange={(e) => setPassword(e.target.value)} required/>
+                                <span className="input-group-text bg-white"><Lock/></span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <div>
-                            <input className="form-check-input" type="checkbox" value="" id="remember"/>
-                            <label className="form-check-label ms-2 mb-3" htmlFor="remember">
-                                Atcerēties mani
-                            </label>
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <input className="form-check-input" type="checkbox" value="" id="remember"/>
+                                <label className="form-check-label ms-2 mb-3" htmlFor="remember">
+                                    Atcerēties mani
+                                </label>
+                            </div>
+                            <div>
+                                <a href="#" className="text-decoration-none text-danger">Aizmirsi paroli?</a>
+                            </div>
                         </div>
-                        <div>
-                            <a href="#" className="text-decoration-none text-danger">Aizmirsi paroli?</a>
+                        <button type="submit" className="btn btn-primary w-100">pieslēgties</button>
+                        <div className="py-3">
+                            Neesi piereģistrējies? <a href="#"
+                                                      className="text-decoration-none text-success">Reģistrēties</a>
                         </div>
-                    </div>
-                    <button type="submit" className="btn btn-primary w-100">pieslēgties</button>
-                </form>
+                    </form>
 
+                </div>
+                <div className="col-4 d-flex bg-white border py-4">
+                    <Image src={Fractal_canopy} objectFit={"contain"} alt="fractal canopy" height={"100%"}/>
+                </div>
             </div>
         </div>
     </>);
