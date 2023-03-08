cbc40c2f006ad09ec68b13c5c79417153fe52751
KrisjanisP <krisjanispetrucena@gmail.com>
2023-03-08 23:36:59 +0200

    minimalist login form

--- website/pages/login.js
+++ website/pages/login.js
@@ -1,8 +1,5 @@
 import {useState} from 'react';
 import {Envelope, Lock} from 'react-bootstrap-icons';
-import Navbar from "../components/Navbar";
-import Image from 'next/image'
-import Fractal_canopy from '../public/fractal-canopy.png'
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
@@ -14,9 +11,8 @@ const LoginForm = () => {
     };
 
     return (<>
-        <Navbar/>
-        <div className="container">
-            <div className={"d-flex justify-content-around mt-5"}>
+        <div className="container vh-100 w-100 d-flex align-items-center pb-5">
+            <div className={"d-flex w-100 justify-content-around mt-5 pb-5"}>
                 <div className="col-5 border p-4">
                     <h4 className="text-center mb-3">Pieslēdzies savam kontam</h4>
                     <form onSubmit={handleSubmit}>
@@ -55,9 +51,9 @@ const LoginForm = () => {
                     </form>
 
                 </div>
-                <div className="col-4 d-flex bg-white border py-4">
-                    <Image src={Fractal_canopy} objectFit={"contain"} alt="fractal canopy" height={"100%"}/>
-                </div>
+                {/*<div className="col-4 d-flex bg-white border py-4">*/}
+                {/*    <Image src={Fractal_canopy} objectFit={"contain"} alt="fractal canopy" height={"100%"}/>*/}
+                {/*</div>*/}
             </div>
         </div>
     </>);
